decf75479144ba87905a6865b098c0f9476fd2ba
Mina <Mina_2@mail.ru>
2019-07-20 16:30:31 +0300

    added test.properties file, added TestPropesLoader class

---
+++ src/main/java/com/mina/mail/ru/cinema/util/TestPropsLoader.java
@@ -0,0 +1,23 @@
+package com.mina.mail.ru.cinema.util;
+
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * Created by Mina on 20.07.2019.
+ */
+public class TestPropsLoader {
+
+    private static String value;
+
+    public static String loadTestProperty (String key) throws IOException {
+        File file = ResourceUtils.getFile("classpath:application_test.properties");
+        Properties properties = new Properties();
+        properties.load(new FileReader(file));
+        return properties.getProperty(key);
+    }
+}
--- src/test/java/com/mina/mail/ru/cinema/FilmConverterTest.java
+++ src/test/java/com/mina/mail/ru/cinema/FilmConverterTest.java
@@ -3,37 +3,45 @@ package com.mina.mail.ru.cinema;
 import com.mina.mail.ru.cinema.dbo.FilmEntity;
 import com.mina.mail.ru.cinema.converter.FilmConverter;
 import com.mina.mail.ru.cinema.dto.FilmDto;
-import com.mina.mail.ru.cinema.dto.FilmTicketDto;
+
+import com.mina.mail.ru.cinema.util.TestPropsLoader;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
 
 /**
  * Created by Mina on 20.05.2019.
  */
 
-public class FilmConverterTest {
-    private static final String filmTitle = "SPIDER_MAN";
+public class FilmConverterTest  {
+
     private final FilmConverter converter = new FilmConverter();
     private static FilmEntity dbo = new FilmEntity();
     private static FilmDto dto = new FilmDto();
 
     @BeforeClass
-    public static void setUpStaticData(){
-        dbo.setTitle(filmTitle);
-        dto.setTitle(filmTitle);
+    public static void setUpStaticData() throws IOException {
+      final String title = TestPropsLoader.loadTestProperty("test.film");
+        dbo.setTitle(title);
+        dto.setTitle(title);
     }
 
     @Test
     public void testAConvertToDto () {
         FilmDto dto = converter.convertToDto(dbo);
-        Assert.assertEquals("Titles are not equal!", dto.getTitle(), "SPIDER_MAN");
+        Assert.assertEquals("Titles are not equal!", dto.getTitle(), "Armageddon");
     }
 
     @Test
     public void testBConvertToDbo () {
         FilmEntity filmEntity = converter.convertToDbo(dto);
-        Assert.assertEquals("Titles are not equal!", filmEntity.getTitle(), "SPIDER_MAN");
+        Assert.assertEquals("Titles are not equal!", filmEntity.getTitle(), "Armageddon");
     }
 
 }
